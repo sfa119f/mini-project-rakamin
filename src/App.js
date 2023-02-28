5080eb0f78c89d0171b9973299f53f1b45d6fbae
Syarifuddin <syarifuddinfa@gmail.com>
2023-02-28 14:27:26 +0700

    delete unused files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
